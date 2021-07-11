3c7d3e734d0f8015f3b16caa81ae88b63fa1562e
ilhmtriixx <ilhamtrii211@gmail.com>
2021-07-11 14:16:07 +0700

    first commit

---
+++ resources/views/document.blade.php
@@ -0,0 +1,5 @@
+@extends('template.template')
+@section('title', 'Document')
+@section('content')
+<hi>Web HamzShop adalah web E-chommers khusus Mahasiswa PHB</hi>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('template.template')
+@section('title', 'Home')
+@section('content')
+<hi>Selamat datang Di web HamzShop</hi>
+@endsection
+
